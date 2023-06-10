3e663b9fcd929ad1129b71ea2584805960d8b1db
ArchanaJain5 <50448011+ArchanaJain5@users.noreply.github.com>
2023-06-10 15:25:20 -0700

    Update Minhash.java

--- project7/Minhash.java
+++ project7/Minhash.java
@@ -8,7 +8,7 @@ import java.util.Random;
 import java.util.Set;
 
 public class Minhash {
-    private static final int numHashes = 30;
+    private static final int numHashes = 32;
 
     public double jaccard(String fileA, String fileB) {
         try {
